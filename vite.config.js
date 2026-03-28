0b5437510d940cefb001d9cb16eed01d0ec96ab5
tzuyuan-0216 <his93112@gmail.com>
2026-03-28 22:47:43 +0800

    pinggy外網連線

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,13 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: true, // 允許透過區域網路 IP 連線
+    port: 5252,  // 這是預設埠號，你也可以改成 8080 等
+    allowedHosts: [
+      'olflj-182-233-192-163.a.free.pinggy.link' // 把報錯訊息中的網址填進去
+    ]
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
